cb9f17af181141c17d10dce84d0dd3a2c9b68c8c
alexandrepasc <alexandre.pascoal@gmail.com>
2019-11-20 22:51:50 +0000

    Remove unused method

--- gsr_draft/lib/view/userView/UserDashboardPage.dart
+++ gsr_draft/lib/view/userView/UserDashboardPage.dart
@@ -90,40 +90,6 @@ class _UserDashboard extends State<UserDashboard> {
             }));
   }
 
-  void _onLoading(BuildContext context) {
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) {
-        return new Container(
-          decoration: new BoxDecoration(
-              color: Colors.transparent,
-              borderRadius: new BorderRadius.circular(10.0)
-          ),
-          width: 150.0,
-          height: 200.0,
-          alignment: AlignmentDirectional.center,
-          child: new Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Center(
-                child: new SizedBox(
-                  height: 50.0,
-                  width: 50.0,
-                  child: new CircularProgressIndicator(
-                    value: null,
-                    strokeWidth: 7.0,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-      },
-    );
-  }
-
   OrientationBuilder _orientationBuilder() =>
       OrientationBuilder(builder: (context, orientation) {
         return GridView.count(
