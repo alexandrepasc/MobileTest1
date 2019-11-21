540dd608b340959610b6142294310b0555bf09c7
alexandrepasc <alexandre.pascoal@gmail.com>
2019-11-21 23:40:44 +0000

    Misc commit
    Draft summary tab component.
    Change app version.

---
+++ gsr_draft/lib/component/SessionDetailSummaryTab.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SessionDetailSummaryTab extends StatelessWidget {
+
+  final String _summary;
+
+  SessionDetailSummaryTab(this._summary);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
