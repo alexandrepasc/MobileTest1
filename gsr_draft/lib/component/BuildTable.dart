d56a1746fc032f756e134c68dc6c631573e628c5
alexandrepasc <alexandre.pascoal@gmail.com>
2019-12-08 22:32:22 +0000

    Table
    Add logic to build the view.

--- gsr_draft/lib/component/BuildTable.dart
+++ gsr_draft/lib/component/BuildTable.dart
@@ -6,6 +6,7 @@ import '../common/RoutePaths.dart' as routes;
 import '../common/Student.dart';
 import '../Locator.dart';
 import '../model/StudentsModel.dart';
+import '../model/UserModelRes.dart';
 import '../service/NavigationService.dart';
 
 final NavigationService _navigationService = locator<NavigationService>();
@@ -101,3 +102,54 @@ _openStudentDetail(StudentModel student, Profile profile) {
 
   _navigationService.navigateTo(routes.studentDetailPageTag, arguments: _profile);
 }
+
+
+
+List<DataRow> buildRowsAdminCoordinatorsList(List<UserModelRes> users, Profile profile) {
+
+  List<DataRow> rows = new List();
+
+  users.forEach((user) {
+    rows.add(
+      DataRow(
+        cells: [
+          _buildCellAdminCoordinatorsList(user.username, user, profile),
+          _buildCellAdminCoordinatorsList(user.name, user, profile),
+          _buildCellAdminCoordinatorsList(user.notes, user, profile)
+        ]
+      )
+    );
+  });
+
+  return rows;
+}
+
+DataCell _buildCellAdminCoordinatorsList(String txt, UserModelRes user, Profile profile) => DataCell(
+    Text(
+      txt,
+      style: TextStyle(
+        fontSize: 17.0,
+      ),
+    ),
+    /*onTap: () {
+      _openCoordinatorDetail(user, profile);
+    }*/
+);
+
+/*_openCoordinatorDetail(UserModelRes user, Profile profile) {
+
+  Student _student = new Student(
+      student.id,
+      student.firstName,
+      student.lastName,
+      student.birthDate,
+      student.description,
+      new ActiveClass(student.activeClass.classId, student.activeClass.className),
+      student.classes
+  );
+
+  Profile _profile = profile;
+  _profile.setStudent(_student);
+
+  _navigationService.navigateTo(routes.studentDetailPageTag, arguments: _profile);
+}*/
--- gsr_draft/lib/view/adminView/AdminCoordinatorsListPage.dart
+++ gsr_draft/lib/view/adminView/AdminCoordinatorsListPage.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:gsr_draft/model/UserModelRes.dart' as userModel;
+
 import '../../common/AdminDrawerListEnum.dart';
 import '../../common/Constants.dart';
 import '../../common/Profile.dart';
-import '../../common/RolesEnum.dart';
 import '../../common/RoutePaths.dart' as routes;
 import '../../component/AdminDrawer.dart';
 import '../../component/AppBar.dart';
 import '../../component/BuildTable.dart';
 import '../../component/LoadingCircle.dart';
 import '../../Locator.dart';
+import '../../model/UserModelRes.dart';
+import '../../service/AuthService.dart';
 import '../../service/NavigationService.dart';
 
 class AdminCoordinatorsListPage extends StatefulWidget {
@@ -39,10 +42,80 @@ class _AdminCoordinatorsListPage extends State<AdminCoordinatorsListPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Text("Coordinators")
+            new FutureBuilder(
+                future: _getCoordinators(widget.profile),
+                builder: (context, AsyncSnapshot snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                      print("AdminCoordinatorsListPage: none");
+                      return loadingCircle();
+                    case ConnectionState.waiting:
+                      print("AdminCoordinatorsListPage: waiting");
+                      return loadingCircle();
+                    case ConnectionState.active:
+                      return loadingCircle();
+                    case ConnectionState.done:
+                      print("AdminCoordinatorsListPage: yap1");
+                      return _getData();
+                    default:
+                      if (snapshot.hasError) {
+                        print("AdminCoordinatorsListPage: error: ${snapshot.error}");
+                        return Text("Error");
+                      }
+                      print("AdminCoordinatorsListPage: nop");
+                      return loadingCircle();
+                  }
+                }
+            )
           ],
         ),
       ),
     );
   }
+
+  UsersModel _users;
+
+  Future<UsersModel> _getCoordinators(Profile profile) async {
+
+    await getCoordinators(profile.getToken()).then((response) {
+
+      if (response.statusCode == 200) {
+
+        UsersModel usersModel = userModel.getUsersFromJson(response.body);
+
+        _users = usersModel;
+
+        return _users;
+
+      } else if (response.statusCode == 401) {
+        print("AdminCoordinatorsListPage: cod 401");
+        _navigationService.navigateToAndRemove(routes.loginPageTag);
+        return null;
+      } else {
+        print("AdminCoordinatorsListPage: " + response.statusCode.toString());
+        return null;
+      }
+    }).catchError((error) {
+      print("AdminCoordinatorsListPage: " + error);
+      return null;
+    });
+  }
+
+  Widget _getData() {
+
+    if (_users.users.length > 0) {
+      return _getTable();
+    } else {
+      return Text("No data");
+    }
+  }
+
+  DataTable _getTable() => DataTable(
+      columns: [
+        topRowCell("Username", 16.0),
+        topRowCell("Name", 16.0),
+        topRowCell("Notes", 16.0)
+      ],
+      rows: [...buildRowsAdminCoordinatorsList(_users.users, widget.profile)]
+  );
 }
