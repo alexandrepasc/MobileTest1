e5757e116b4bbadabf73cfd3f28e8d4a3509db78
alexandrepasc <alexandre.pascoal@gmail.com>
2019-12-10 22:20:20 +0000

    Add coach profile structure

---
+++ gsr_draft/lib/common/Coach.dart
@@ -0,0 +1,49 @@
+class Coach {
+
+  String _id;
+  String _firstName;
+  String _lastName;
+  String _description;
+  String _authId;
+
+  getId() {
+    return _id;
+  }
+
+  setId(String _id) {
+    this._id = _id;
+  }
+
+  getFirstName() {
+    return _firstName;
+  }
+
+  setFirstName(String _firstName) {
+    this._firstName = _firstName;
+  }
+
+  getLastName() {
+    return _lastName;
+  }
+
+  setLastName(String _lastName) {
+    this._lastName = _lastName;
+  }
+
+  getDescription() {
+    return _description;
+  }
+
+  setDescription(String _description) {
+    this._description = _description;
+  }
+
+  getAuthId() {
+    return _authId;
+  }
+
+  setAuthId(String _authId) {
+    this._authId = _authId;
+  }
+
+}
