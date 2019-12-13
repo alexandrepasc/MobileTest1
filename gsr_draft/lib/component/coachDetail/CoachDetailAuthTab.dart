ee9a021a46ea701e395c2680cbb3148f14558034
alexandrepasc <alexandre.pascoal@gmail.com>
2019-12-13 23:42:15 +0000

    View content
    Add the form components, and logic to build it.

--- gsr_draft/lib/component/coachDetail/CoachDetailAuthTab.dart
+++ gsr_draft/lib/component/coachDetail/CoachDetailAuthTab.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 
 import 'package:gsr_draft/model/UserModelRes.dart' as userModel;
 
+import '../../common/Auth.dart';
 import '../../common/Constants.dart';
 import '../../common/Profile.dart';
 import '../../common/RolesEnum.dart';
 import '../../common/RoutePaths.dart' as routes;
+import '../../component/LoadingCircle.dart';
 import '../../Locator.dart';
 import '../../model/UserModelRes.dart';
 import '../../service/AuthService.dart';
@@ -25,57 +27,243 @@ ContentData contentData = new ContentData();
 class _CoachDetailAuthTab extends State<CoachDetailAuthTab> {
   final NavigationService _navigationService = locator<NavigationService>();
 
-  bool _readOnly = true;
+  Auth _auth;
 
-  UserModelRes _userModel;
+  bool _readOnly = true;
 
   @override
   void initState() {
     super.initState();
+
+    _auth = widget.profile.getAuth();
+
+    contentData.setUsername(_auth.getUsername());
+    contentData.setName(_auth.getName());
+    contentData.setNotes(_auth.getNotes());
   }
+
+  TextEditingController usernameController = new TextEditingController();
+  TextEditingController nameController = new TextEditingController();
+  TextEditingController notesController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
 
-    OrientationBuilder _orientationBuilder() => OrientationBuilder(
-        builder: (context, orientation) {
-          return GridView.count(
-            padding: EdgeInsets.all(15.0),
-            crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
-            children: <Widget>[],
-          );
-        }
+    usernameController.text = contentData.getUsername();
+    TextFormField _getUsernameInput() => TextFormField(
+      controller: usernameController,
+      onChanged: (text) {
+        contentData.setUsername(text);
+      },
+      keyboardType: TextInputType.text,
+      maxLines: 1,
+      readOnly: _readOnly,
+      decoration: formDecoration(),
     );
+
+    nameController.text = contentData.getName();
+    TextFormField _getNameInput() => TextFormField(
+      controller: nameController,
+      onChanged: (text) {
+        contentData.setName(text);
+      },
+      keyboardType: TextInputType.text,
+      maxLines: 1,
+      readOnly: _readOnly,
+      decoration: formDecoration(),
+    );
+
+    notesController.text = contentData.getNotes();
+    TextField _getNotesInput() => TextField(
+      onChanged: (text) {
+        contentData.setNotes(text);
+      },
+      keyboardType: TextInputType.multiline,
+      maxLines: 5,
+      controller: notesController,
+      readOnly: _readOnly,
+      decoration: formDecoration(),
+    );
+
+    TextFormField _getRoleInput() => TextFormField(
+      controller: new TextEditingController(text: "Coach"),
+      keyboardType: TextInputType.text,
+      maxLines: 1,
+      readOnly: true,
+      decoration: formDecoration(),
+    );
+
+
+
+    Card _getAuthData() => Card(
+      child: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          logo,
+          SizedBox(height: mediumHeight),
+          _infoText("Username:"),
+          SizedBox(height: smallHeight),
+          _getUsernameInput(),
+          SizedBox(height: buttonHeight),
+          _infoText("Name:"),
+          SizedBox(height: smallHeight),
+          _getNameInput(),
+          SizedBox(height: buttonHeight),
+          _infoText("Notes:"),
+          SizedBox(height: smallHeight),
+          _getNotesInput(),
+          SizedBox(height: buttonHeight),
+          _infoText("Role:"),
+          SizedBox(height: smallHeight),
+          _getRoleInput(),
+          SizedBox(height: buttonHeight),
+
+        ],
+      ),
+    );
+
+
 
     return Scrollbar(
-      child: _orientationBuilder(),
+      child: Center(
+        child: ListView(
+          children: <Widget>[
+            Center(
+              child: Container(
+                width: 350.0,
+                child: _getAuthData(),
+              ),
+            ),
+            _checkPermissions()
+          ],
+        )
+      )
     );
   }
 
-  Future _apiGetAuth(String token, String id) async {
 
-    await getPost(id, token).then((response) {
 
-      if (response.statusCode == 200) {
+  final logo = CircleAvatar(
+    backgroundColor: Colors.transparent,
+    radius: bigRadius,
+    child: studentLogo,
+  );
 
-        _userModel = userModel.postFromJson(response.body);
+  Text _infoText(String txt) => Text(
+    txt,
+    style: TextStyle(
+        color: appDarkRedColor,
+        fontWeight: FontWeight.bold,
+        fontSize: 16.0),
+    textAlign: TextAlign.left,
+  );
 
-        return _userModel;
 
-      } else if (response.statusCode == 401) {
-        print("CoachDetailAuthTab: cod 401");
-        _navigationService.navigateToAndRemove(routes.loginPageTag);
-        return null;
-      } else {
-        print("CoachDetailAuthTab: " + response.statusCode.toString());
-        return null;
-      }
 
-    }).catchError((error) {
-      print("CoachDetailAuthTab: " + error);
-      return null;
+  int _index = 0;
+
+  setEdit(bool _notRead, int _show) {
+    _readOnly = _notRead;
+    _index = _show;
+
+    setState(() {
+
     });
+    print("setEdit");
   }
+
+  _checkPermissions() {
+    if (hasCoordinator(widget.profile.getRoles())) {
+      return _getButtons();
+    } else {
+      return SizedBox(height: buttonHeight);
+    }
+  }
+
+  _getButtons() {
+    switch (_index) {
+      case 0:
+        return _getEditButton();
+      case 1:
+        return _getSaveCancelButton();
+      default:
+        return null;
+    }
+  }
+
+  Column _getEditButton() => Column(
+    children: <Widget>[
+      SizedBox(height: buttonHeight),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          FloatingActionButton(
+            child: Icon(Icons.edit),
+            backgroundColor: appDarkRedColor,
+            onPressed: () {
+              setEdit(false, 1);
+            },
+            heroTag: "edit",
+          ),
+        ],
+      ),
+    ],
+  );
+
+  Column _getSaveCancelButton() => Column(
+    children: <Widget>[
+      SizedBox(height: buttonHeight),
+      Row(
+        children: <Widget>[
+          FloatingActionButton(
+            child: Icon(Icons.check),
+            backgroundColor: Colors.green,
+            onPressed: () {
+
+              loadingCircle();
+
+              //_apiUpdateCoach(widget.profile.getToken(), widget.profile.getCoach().getId(), contentData);
+
+              setEdit(true, 0);
+            },
+            heroTag: "save",
+          ),
+          FloatingActionButton(
+            child: Icon(Icons.clear),
+            backgroundColor: Colors.red,
+            onPressed: () {
+
+              contentData.setUsername(_auth.getUsername());
+              contentData.setName(_auth.getName());
+              contentData.setNotes(_auth.getNotes());
+
+              setEdit(true, 0);
+            },
+            heroTag: "cancel",
+          ),
+        ],
+      ),
+    ],
+  );
+
+
+
+  InputDecoration formDecoration() => InputDecoration(
+    hintText: userNameHintText,
+    hintStyle: TextStyle(color: Colors.black38),
+    contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(32.0),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(32.0),
+      borderSide: BorderSide(color: appDarkRedColor, width: 3.0),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(32.0),
+      borderSide: BorderSide(color: appDarkRedColor, width: 3.0),
+    ),
+  );
 }
 
 class ContentData {
