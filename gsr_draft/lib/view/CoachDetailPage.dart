b7942747d9c5d51bd1c2c001aafff7d6e6998c87
alexandrepasc <alexandre.pascoal@gmail.com>
2019-12-13 21:29:27 +0000

    Auth api
    Add get auth to coach detail view.
    Add logic to pass data to auth tab.

--- gsr_draft/lib/view/CoachDetailPage.dart
+++ gsr_draft/lib/view/CoachDetailPage.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'package:gsr_draft/model/UserModelRes.dart' as userModel;
+
 import '../common/AdminDrawerListEnum.dart';
+import '../common/Auth.dart';
 import '../common/Coach.dart';
 import '../common/Constants.dart';
 import '../common/Profile.dart';
 import '../common/RolesEnum.dart';
 import '../common/RoutePaths.dart' as routes;
+import '../component/coachDetail/CoachDetailAuthTab.dart';
 import '../component/coachDetail/CoachDetailProfileTab.dart';
 import '../component/AdminDrawer.dart';
 import '../component/AppBar.dart';
 import '../component/LoadingCircle.dart';
 import '../Locator.dart';
+import '../model/UserModelRes.dart';
+import '../service/AuthService.dart';
 import '../service/NavigationService.dart';
 
 class CoachDetailPage extends StatefulWidget {
@@ -50,9 +56,56 @@ class _CoachDetailPage extends State<CoachDetailPage> {
     switch (x) {
       case 0:
         return CoachDetailProfileTab(profile: widget.profile,);
-      /*case 1:
-        return SessionDetailAttendanceTab(profile: widget.profile,);*/
+      case 1:
+        return _getAuthTab();
     }
+  }
+
+
+
+  UserModelRes _userModel;
+
+  Widget _getAuthTab() {
+
+    _apiGetAuth(widget.profile.getToken(), widget.profile.getCoach().getAuthId());
+
+    Auth auth = new Auth(
+        _userModel.id,
+        _userModel.username,
+        _userModel.name,
+        _userModel.notes,
+        _userModel.roles
+    );
+
+    Profile profile = widget.profile;
+    profile.setAuth(auth);
+
+    return CoachDetailAuthTab(profile: profile,);
+  }
+
+  Future _apiGetAuth(String token, String id) async {
+
+    await getPost(id, token).then((response) {
+
+      if (response.statusCode == 200) {
+
+        _userModel = userModel.postFromJson(response.body);
+        print(_userModel.id);
+        return _userModel;
+
+      } else if (response.statusCode == 401) {
+        print("CoachDetailAuthTab: cod 401");
+        _navigationService.navigateToAndRemove(routes.loginPageTag);
+        return null;
+      } else {
+        print("CoachDetailAuthTab: " + response.statusCode.toString());
+        return null;
+      }
+
+    }).catchError((error) {
+      print("CoachDetailAuthTab: " + error);
+      return null;
+    });
   }
 
   BottomNavigationBar _setBottomNavBar() => BottomNavigationBar(
